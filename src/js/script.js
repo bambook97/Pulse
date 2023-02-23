9e46e704f4bf4d1cd385384c3c7ea9c225ebf1d7
Nikita <bambook97@gmail.com>
2023-02-23 10:25:56 +0600

    Add modal

--- src/js/script.js
+++ src/js/script.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
             {
                 breakpoint: 992,
                 settings: {
-                    dots: true,
+                    
                     arrows: false
                 }
             }
@@ -35,4 +35,20 @@ $(document).ready(function(){
 
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
+
+    //Modal
+
+    $('[data-modal=consultation]').on('click', function (){
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+    $('.modal__close').on('click',function(){
+        $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+    });
+    
+    $('.button_mini').each(function(i){
+         $(this).on('click', function(){
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+         })
+    });
   });
